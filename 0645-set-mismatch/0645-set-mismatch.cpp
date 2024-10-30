03197549ac0fc9b23750ee29901df5a86d044853
tanned366 <169049907+tanned366@users.noreply.github.com>
2024-10-30 17:11:36 +0530

    Time: 5 ms (54.73%), Space: 24.9 MB (53.28%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,17 +1,20 @@
-
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        vector<int> v(n+1,0);
-        int missing=0, duplicate =0;
-        for(int i =0; i<n; i++){
-            v[nums[i]]++;
+        vector<int> ans();
+        int sum = 0;
+        int sumTotal = (n*(n + 1))/2;
+        sort(nums.begin(), nums.end());
+        int duplicate;
+        for (int i = 0; i < n - 1; i++) {
+            sum += nums[i]; 
+            if (nums[i] == nums[i + 1]) {
+                duplicate = nums[i];
+            }
         }
-        for(int i =1;i<v.size();i++){
-            if(v[i]==2) duplicate = i;
-            if(v[i]==0) missing = i;
-        }
-        return {duplicate,missing};
+        sum += nums[n - 1];
+        int missingNo = sumTotal - (sum - duplicate);
+        return{duplicate, missingNo};
     }
-}; 
+};
