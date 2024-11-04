e82f1fdf596127af383a5d2b6395838bd074a60c
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2024-11-04 22:44:21 +0530

    Update 0151-reverse-words-in-a-string.cpp

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n=s.length();
-        reverse(s.begin(), s.end());
+        reverse(s.begin(), s.end());    // revere the whole string
         string ans = "";
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n; i++) {        // for every character in string
             string word = "";
-            while(i < n && s[i] != ' ') {
-                word += s[i];
+            while(i < n && s[i] != ' ') {    // until space is encountered or str length is completed 
+                word += s[i];            // store every word separately
                 i++;
             }
-            reverse(word.begin(), word.end());
-            if(word.length() > 0) {
+            reverse(word.begin(), word.end());    // reverse the word
+            if(word.length() > 0) {            // checking if a string is empty because if it is it will cause error and print space
                 ans += " " + word;
             }
         }
