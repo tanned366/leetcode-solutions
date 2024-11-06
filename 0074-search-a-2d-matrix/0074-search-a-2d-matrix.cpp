56e950c06e3837d25583cce440abac3df5fa561f
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2024-11-06 23:57:24 +0530

    Time: 0 ms (100%), Space: 12.1 MB (77.38%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,31 +5,21 @@ public:
         int st = 0, end = n-1;
         while(st <= end) {
             int mid = st + (end-st)/2;
-            if(target == matrix[row][mid]) {
-                return true;
-            } else if(target > matrix[row][mid]) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+            if(target == matrix[row][mid]) return true;
+            else if(target > matrix[row][mid]) st = mid + 1;
+            else end = mid - 1;
         }
         return false;
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty() || matrix[0].empty()) {
-            return false;
-        }
+        if(matrix.empty() || matrix[0].empty()) return false;
         int m = matrix.size(), n = matrix[0].size();
         int startRow = 0, endRow = m-1; 
         while (startRow <= endRow) {
             int midRow = startRow + (endRow-startRow)/2;
-            if (matrix[midRow][0] <= target && target <= matrix[midRow][n-1] ) {
-                return searchRowWise(matrix, target, midRow);
-            } else if (target >= matrix[midRow][n-1]) {
-                startRow = midRow + 1;
-            } else {
-                endRow = midRow - 1;
-            }
+            if (matrix[midRow][0] <= target && target <= matrix[midRow][n-1] ) return searchRowWise(matrix, target, midRow);
+            else if (target >= matrix[midRow][n-1]) startRow = midRow + 1;
+            else endRow = midRow - 1;
         }
         return false;
     }
