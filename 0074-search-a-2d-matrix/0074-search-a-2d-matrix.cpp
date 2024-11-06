a8e286c1f25af633f9dda166cf6b66c2cc983c93
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2024-11-06 23:27:39 +0530

    Time: 0 ms (100%), Space: 12.2 MB (39.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {    //Staircase Search
+    bool searchRowWise(vector<vector<int>>& matrix, int target, int row) {
+        int n = matrix[0].size();
+        int st = 0, end = n-1;
+        while(st <= end) {
+            int mid = st + (end-st)/2;
+            if(target == matrix[row][mid]) {
+                return true;
+            } else if(target > matrix[row][mid]) {
+                st = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if(matrix.empty() || matrix[0].empty()) {
             return false;
         }
-        int i = 0, j = matrix[0].size() - 1;
-        while (i < matrix.size() && j >= 0) {
-            if (matrix[i][j] == target) {
-                return true;
-            } else if (matrix[i][j] > target) {
-                j--;
+        int m = matrix.size(), n = matrix[0].size();
+        int startRow = 0, endRow = m-1; 
+        while (startRow <= endRow) {
+            int midRow = startRow + (endRow-startRow)/2;
+            if (matrix[midRow][0] <= target && target <= matrix[midRow][n-1] ) {
+                return searchRowWise(matrix, target, midRow);
+            } else if (target >= matrix[midRow][n-1]) {
+                startRow = midRow + 1;
             } else {
-                i++;
+                endRow = midRow - 1;
             }
         }
         return false;
