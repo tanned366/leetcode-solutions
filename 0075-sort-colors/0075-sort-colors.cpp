b1384bee752f1f4ffe408fee8c12dd8f86aaeddd
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2024-11-11 23:48:34 +0530

    Update 0075-sort-colors.cpp

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,3 +1,4 @@
+// DNF(Dutch National Flag) Algorithm
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
@@ -7,11 +8,38 @@ public:
             if(nums[mid] == 0) {
                 swap(nums[low], nums[mid]);
                 low++; mid++;
-            } else if(nums[mid] == 1) mid++;
-            else {
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else {
                 swap(nums[mid], nums[high]);
                 high--;
             }
         }
+    }
+};
+
+// Optimal
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int count0=0, count1=0, count2=0;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == 0) count0++;
+            else if(nums[i] == 1) count1++;
+            else count2++;
+        }
+        int idx=0;
+        for(int i=0; i<count0; i++) nums[idx++]=0;
+        for(int i=0; i<count1; i++) nums[idx++]=1;
+        for(int i=0; i<count2; i++) nums[idx++]=2;
+    }
+};
+
+// Brute-Forece
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
     }
 };
