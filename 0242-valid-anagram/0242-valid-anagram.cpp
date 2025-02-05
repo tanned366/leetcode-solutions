09135cb4434c815b17446faa5cd048f477ba7f96
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-02-05 21:49:08 +0530

    Time: 0 ms (100%), Space: 8.9 MB (100%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,9 +4,18 @@ public:
         if (s.length() != t.length()) {
             return false;           // no need to check further
         }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(t == s) return true;
-        return false;
+        int count[26] = {0};
+        for(int i=0; i<s.length(); i++) {
+            int idx = s[i] - 'a';       // to find idx of character
+            count[idx]++;
+        }
+        for(int i=0; i<t.length(); i++) {
+            int idx = t[i] - 'a';
+            if(count[idx] == 0) {
+                return false;
+            }
+            count[idx]--;
+        }
+        return true;
     }
 };
