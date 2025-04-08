337b06d95dad6577aefe864e2a658528558b5df4
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-04-08 17:59:11 +0530

    Time: 0 ms (100%), Space: 8.5 MB (57.8%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
@@ -6,14 +6,10 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-    int pos=1;
-    struct ListNode *temp=head;
-    while(temp->next!=NULL) {
-        temp=temp->next;
-        pos++;
+    struct ListNode *slow=head, *fast=head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    temp=head;
-    for(int i=0; i<pos/2; i++)
-        temp = temp->next;
-    return temp;
+    return slow;
 }
