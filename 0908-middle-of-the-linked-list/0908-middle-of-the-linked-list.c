2fc469b41886dd8674d85abdb8237b08eb3a96df
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-04-08 18:00:05 +0530

    Update 0908-middle-of-the-linked-list.c

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
@@ -13,3 +13,15 @@ struct ListNode* middleNode(struct ListNode* head) {
     }
     return slow;
 }
+struct ListNode* middleNode(struct ListNode* head) {
+    int pos=1;
+    struct ListNode *temp=head;
+    while(temp->next!=NULL) {
+        temp=temp->next;
+        pos++;
+    }
+    temp=head;
+    for(int i=0; i<pos/2; i++)
+        temp = temp->next;
+    return temp;
+}
