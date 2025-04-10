2ae474720aa0db9cdd20e180d881ab87a023f4e6
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-04-10 11:36:49 +0530

    Update 1528-kids-with-the-greatest-number-of-candies.cpp

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -18,3 +18,17 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxCandy = 0;
+        for (int candy : candies)
+            maxCandy = max(candy, maxCandy);
+        vector<bool> ans(candies.size(), false);
+        for (int i = 0; i < candies.size(); i++)
+            if ((candies[i] + extraCandies) >= maxCandy)
+                ans[i]=true;
+        return ans;
+    }
+};
