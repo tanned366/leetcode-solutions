2af73d47f637de54fd6a0a79635dae75768b25da
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-07-01 00:47:30 +0530

    Time: 2 ms (77.54%), Space: 7.8 MB (63.91%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        int firstnum=0;
-        int secondnum=1;
-        int fibonum;
         if(n==0) return 0;
         if(n==1) return 1;
+        int firstnum=0, secondnum=1, fibonum;
         for(int i=2; i<=n; i++){
             fibonum = firstnum + secondnum;
             firstnum = secondnum;
