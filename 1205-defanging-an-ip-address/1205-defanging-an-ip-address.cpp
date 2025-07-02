93ab7c1a4031df92bfe2dace42aa7a60ec5a5c4f
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-07-02 13:59:40 +0530

    Time: 0 ms (100%), Space: 7.6 MB (93.76%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        int n=address.size();
-        for(int i=0; i<n; i++) {
-            if(address[i] == '.') {
-                address.replace(i, 1, "[.]");
-                n +=2;
-                i +=2;
-            }
+        string result="";
+        for(char ch: address){
+            if(ch == '.')
+                result += "[.]";
+            else    
+                result += ch;
         }
-        return address;
+        return result;
     }
 };
