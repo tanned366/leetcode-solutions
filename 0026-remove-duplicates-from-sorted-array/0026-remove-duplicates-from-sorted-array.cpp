7bdfbf28fa732ecd2abb488dd52ec854692b3c53
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-07-05 00:17:04 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -12,3 +12,18 @@ public:
         return idx;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=0; j<nums.size(); j++){
+            if(nums[j] > nums[i]){
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+};
