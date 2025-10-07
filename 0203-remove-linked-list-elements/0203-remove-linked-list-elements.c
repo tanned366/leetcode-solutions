47e3b26932d977a693053285c29fce986a7b225b
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-10-07 18:57:14 +0530

    Time: 0 ms (100%), Space: 12.5 MB (82.76%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.c
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.c
@@ -13,49 +13,10 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
                 head = head->next;
             else
                 ptemp->next = temp->next;
-            temp = temp->next;
         } else {
             ptemp = temp;
-            temp = temp->next;
         }
-    }
-    return head;
-}
-
-struct ListNode* removeElements(struct ListNode* head, int val) {
-    while (head != NULL && head->val == val){
-        struct ListNode* todelete = head;
-        head=head->next;
-    }
-    struct ListNode *temp = head, *ptemp = NULL;
-    while (temp != NULL) {
-        if (temp->val == val) {
-            ptemp->next = temp->next;
-            temp = ptemp->next;
-        } else {
-            ptemp = temp;
-            temp = temp->next;
-        }
-    }
-    return head;
-}
-
-struct ListNode* removeElements(struct ListNode* head, int val) {
-    while (head != NULL && head->val == val){
-        struct ListNode* todelete = head;
-        head=head->next;
-        free(todelete);
-    }
-    struct ListNode *temp = head, *ptemp = NULL;
-    while (temp != NULL) {
-        if (temp->val == val) {
-            ptemp->next = temp->next;
-            free(temp);
-            temp = ptemp->next;
-        } else {
-            ptemp = temp;
-            temp = temp->next;
-        }
+        temp = temp->next;
     }
     return head;
 }
