ade4cb400c43f2830ac4ff68889198fbef0a4219
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-10-09 20:50:29 +0530

    Time: 0 ms (100%), Space: 19.3 MB (99.95%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,22 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for(int val : nums) {    //(int i=0; i<nums.size(); i++) 
-            ans = ans ^ val;     //int val = nums[i];
+        for(int i=0; i<nums.size(); i++) { 
+            int val = nums[i];
+            ans = ans ^ val;    
         }
         return ans;
-    }
-};
-
-
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        for(int i=0; i<n-1; i+=2) {
-            if(nums[i] != nums[i+1]) return nums[i];
-        }
-        return nums[n-1];
     }
 };
